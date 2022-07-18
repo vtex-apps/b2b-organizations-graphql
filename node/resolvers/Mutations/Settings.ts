6b74936a917767441a9529cf5bbba43ee6f8cffe
Paulo Ribeiro <pauloribeiro@Paulos-Mac-mini.local>
2022-07-18 19:42:25 -0400

    Query and Mutation working, updated document if one exists otherwise creates one

--- node/resolvers/Mutations/Settings.ts
+++ node/resolvers/Mutations/Settings.ts
@@ -1,59 +1,86 @@
 import {
-    B2B_SETTINGS_DATA_ENTITY,
-    B2B_SETTINGS_SCHEMA_VERSION,
-  } from '../../mdSchema'
-  import GraphQLError from '../../utils/GraphQLError'
-  import checkConfig from '../config'
+  B2B_SETTINGS_DATA_ENTITY,
+  B2B_SETTINGS_SCHEMA_VERSION,
+  B2B_SETTINGS_FIELDS
+} from '../../mdSchema'
+import GraphQLError from '../../utils/GraphQLError'
+import checkConfig from '../config'
 
-  const B2BSettings = {
-    saveB2BSettings: async (
-      _: void,
-      {
-        input: { autoApprove, defaultPaymentTerms, defaultPriceTables },
-      }: { input: B2BSettingsInput },
-      ctx: Context
-    ) => {
-      const {
-        clients: { masterdata },
-        vtex: { logger },
-      } = ctx
-  
-      // create schema if it doesn't exist
-      await checkConfig(ctx)
-  
-      try {
-        const b2bSettings = {
-            autoApprove,
-            defaultPaymentTerms, 
-            defaultPriceTables
-        }
+const B2BSettings = {
+  saveB2BSettings: async (
+    _: void,
+    {
+      input: { autoApprove, defaultPaymentTerms, defaultPriceTables }, 
+      page,
+      pageSize
+    }: {
+      input: B2BSettingsInput, page: number
+      pageSize: number
+    },
+    ctx: Context
+  ) => {    const {
+      clients: { masterdata },
+      vtex: { logger },
+    } = ctx
 
-       
-        const saveB2BSettingResult = await masterdata.createDocument({
+    // create schema if it doesn't exist
+    await checkConfig(ctx)
+
+    try {
+      const b2bSettings = {
+        autoApprove,
+        defaultPaymentTerms,
+        defaultPriceTables
+      }
+      
+      const documentB2BSettingResult = await masterdata.searchDocuments({
+        dataEntity: B2B_SETTINGS_DATA_ENTITY,
+        fields: ['id', 'accountId' , ...B2B_SETTINGS_FIELDS],
+        pagination: { page, pageSize },
+        schema: B2B_SETTINGS_SCHEMA_VERSION,
+      })
+
+      if(!!documentB2BSettingResult.length) {
+        const document = documentB2BSettingResult as any 
+
+        await masterdata.updatePartialDocument({
+          id: document[0].id,
           dataEntity: B2B_SETTINGS_DATA_ENTITY,
           fields: b2bSettings,
           schema: B2B_SETTINGS_SCHEMA_VERSION
         })
-
+        
         return {
-          href: saveB2BSettingResult.Href,
-          id: saveB2BSettingResult.Id,
           status: 'success',
-        }
-      } catch (e) {
-        logger.error({
-          message: 'saveB2BSettings-error',
-          error: e,
-        })
-        if (e.message) {
-          throw new GraphQLError(e.message)
-        } else if (e.response?.data?.message) {
-          throw new GraphQLError(e.response.data.message)
-        } else {
-          throw new GraphQLError(e)
+          message: 'Document Updated'
         }
       }
-    },
-  }
 
-  export default B2BSettings
+      const saveB2BSettingResult = await masterdata.createDocument({
+        dataEntity: B2B_SETTINGS_DATA_ENTITY,
+        fields: b2bSettings,
+        schema: B2B_SETTINGS_SCHEMA_VERSION
+      })
+
+      return {
+        href: saveB2BSettingResult.Href,
+        id: saveB2BSettingResult.Id,
+        status: 'success',
+      }
+    } catch (e) {
+      logger.error({
+        message: 'saveB2BSettings-error',
+        error: e,
+      })
+      if (e.message) {
+        throw new GraphQLError(e.message)
+      } else if (e.response?.data?.message) {
+        throw new GraphQLError(e.response.data.message)
+      } else {
+        throw new GraphQLError(e)
+      }
+    }
+  },
+}
+
+export default B2BSettings
