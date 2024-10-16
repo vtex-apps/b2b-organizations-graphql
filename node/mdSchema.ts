9aba0fa281f8b4625f660239459b1ced94637836
giurigaud <giuliana.rigaud@cubos.io>
2024-10-16 16:52:42 -0300

    feat: added tradename and id on org search queries

--- node/mdSchema.ts
+++ node/mdSchema.ts
@@ -15,7 +15,7 @@ export const ORGANIZATION_REQUEST_FIELDS = [
   'sellers',
   'customFields',
 ]
-export const ORGANIZATION_REQUEST_SCHEMA_VERSION = 'v0.1.1'
+export const ORGANIZATION_REQUEST_SCHEMA_VERSION = 'v0.1.2'
 
 export const ORGANIZATION_DATA_ENTITY = 'organizations'
 export const ORGANIZATION_FIELDS = [
@@ -33,7 +33,7 @@ export const ORGANIZATION_FIELDS = [
   'customFields',
   'permissions',
 ]
-export const ORGANIZATION_SCHEMA_VERSION = 'v0.0.8'
+export const ORGANIZATION_SCHEMA_VERSION = 'v0.0.9'
 
 export const COST_CENTER_DATA_ENTITY = 'cost_centers'
 export const COST_CENTER_FIELDS = [
@@ -112,7 +112,14 @@ export const schemas = [
           title: 'Custom Fields',
         },
       },
-      'v-indexed': ['name', 'b2bCustomerAdmin', 'status', 'created'],
+      'v-indexed': [
+        'id',
+        'tradeName',
+        'name',
+        'b2bCustomerAdmin',
+        'status',
+        'created',
+      ],
       'v-immediate-indexing': true,
       'v-cache': false,
     },
@@ -177,7 +184,7 @@ export const schemas = [
           },
         },
       },
-      'v-indexed': ['name', 'status', 'created'],
+      'v-indexed': ['id', 'tradeName', 'name', 'status', 'created'],
       'v-immediate-indexing': true,
       'v-cache': false,
     },
