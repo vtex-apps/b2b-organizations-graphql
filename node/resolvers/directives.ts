72e268ac5a6f364f7f0933f2d5d19fb5d328a39c
Enzo Mercanti <131273915+enzomerca@users.noreply.github.com>
2025-05-27 07:19:49 -0300

    chore: update dependency major version (#198)

--- node/resolvers/directives.ts
+++ node/resolvers/directives.ts
@@ -2,13 +2,9 @@ import { WithSession } from './directives/withSession'
 import { WithPermissions } from './directives/withPermissions'
 import { ValidateAdminUserAccess } from './directives/validateAdminUserAccess'
 import { ValidateStoreUserAccess } from './directives/validateStoreUserAccess'
-import { CheckAdminAccess } from './directives/checkAdminAccess'
-import { CheckUserAccess } from './directives/checkUserAccess'
 import { AuditAccess } from './directives/auditAccess'
 
 export const schemaDirectives = {
-  checkAdminAccess: CheckAdminAccess as any,
-  checkUserAccess: CheckUserAccess as any,
   validateAdminUserAccess: ValidateAdminUserAccess as any,
   validateStoreUserAccess: ValidateStoreUserAccess as any,
   withPermissions: WithPermissions as any,
